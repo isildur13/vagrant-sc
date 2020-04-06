a5404ba9e3e9338509df0d0e1eda7ed2d0394ddb
Yash Panchal <panchalyash13@gmail.com>
2020-04-06 22:09:56 +0530

    Jenkins agent Service created..

--- basic-provision.sh
+++ basic-provision.sh
@@ -13,7 +13,30 @@ apt install python -y
 wget https://dl.google.com/go/go1.14.1.linux-amd64.tar.gz
 tar -C /usr/local -xzf go1.14.1.linux-amd64.tar.gz
 
-
 cd /bin 
-
 ln -s /usr/local/go/bin/go go
+
+apt install openjdk-8-jre-headless -y
+apt install openjdk-8-jdk -y
+
+cat > jenkinsagent.service <<- "EOF"
+
+[Unit]
+Description=Jenkinsagent
+
+[Service]
+Type=forking
+ExecStart=/etc/jenkinsagent/jenkinsagent.sh
+
+[Install]
+WantedBy=multi-user.target
+
+EOF
+
+mv jenkinsagent.service /etc/systemd/system/
+mkdir /etc/jenkinsagent
+touch /etc/jenkinsagent/jenkinsagent.sh
+chmod 777 /etc/jenkinsagent/jenkinsagent.sh
+systemctl enable jenkinsagent
+systemctl restart jenkinsagent
+
