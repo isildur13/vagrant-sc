60f894ed2f042d0a92c1ea7734fed605e912032a
panchalyash13 <panchalyash13@gmail.com>
2020-04-07 22:29:25 +0530

    Modified for time..

--- basic-provision.sh
+++ basic-provision.sh
@@ -8,35 +8,9 @@ cat /etc/ssh/sshd_config
 echo "------------______RESTARTING SSHD_____--------------"
 systemctl restart sshd ssh
 apt-get update
+timedatectl set-ntp true
+systemctl status systemd-timesyncd
 apt install python -y
-
-wget https://dl.google.com/go/go1.14.1.linux-amd64.tar.gz
-tar -C /usr/local -xzf go1.14.1.linux-amd64.tar.gz
-
-cd /bin 
-ln -s /usr/local/go/bin/go go
-
 apt install openjdk-8-jre-headless -y
 apt install openjdk-8-jdk -y
-
-cat > jenkinsagent.service <<- "EOF"
-
-[Unit]
-Description=Jenkinsagent
-
-[Service]
-Type=forking
-ExecStart=/etc/jenkinsagent/jenkinsagent.sh
-
-[Install]
-WantedBy=multi-user.target
-
-EOF
-
-mv jenkinsagent.service /etc/systemd/system/
-mkdir /etc/jenkinsagent
-touch /etc/jenkinsagent/jenkinsagent.sh
-chmod 777 /etc/jenkinsagent/jenkinsagent.sh
-systemctl enable jenkinsagent
-systemctl restart jenkinsagent
 
